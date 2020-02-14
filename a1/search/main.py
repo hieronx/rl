af1d48a8680a4942f2e3fcac58c76155a1ceb546
Jeroen Offerijns <jeroen@offerijns.com>
2020-02-14 13:47:32 +0100

    feat: set up dijkstra skeleton

--- a1/search/main.py
+++ a1/search/main.py
@@ -8,7 +8,6 @@ from hexboard import HexBoard
 
 char_to_row_idx = {'a': 0, 'b': 1, 'c': 2, 'd': 3}
 
-
 class HexMinimax:
 
     def __init__(self, size, depth, eval_method):
@@ -60,15 +59,16 @@ class HexMinimax:
 
         best_score = math.inf if not maximizing else -math.inf
         best_move = None
+
         total_nodes_searched = 0
         total_cutoffs = 0
+
         for move in moves:
             new_board = board.make_move(move, color)
             _, score, nodes_searched, cutoffs = self.alpha_beta_search(new_board, depth - 1, board.get_opposite_color(color), lower_bound_a, upper_bound_b, not maximizing)
             total_nodes_searched += nodes_searched
             total_cutoffs += cutoffs
 
-            
             if maximizing and score > best_score:
                 best_move = move
                 best_score = score
@@ -91,7 +91,6 @@ class HexMinimax:
 
         return (best_move, best_score, total_nodes_searched, total_cutoffs)
 
-
     def get_next_move(self, board, depth, color):
         lower_bound_a = -math.inf
         upper_bound_b = math.inf
@@ -110,33 +109,34 @@ class HexMinimax:
 
         return empty_coordinates
 
+    def find_shortest_path_to_border(self, board, color):
+        source_coords = board.get_source_coordinates(color)
+        min_score = math.inf
+        for from_coord in source_coords:
+            # Only count nodes without placed positions of this color
+            score = np.random.uniform(0, 10) # TODO: replace this
+
+            if score < min_score:
+                min_score = score
+
+        return min_score
+
     def evaluate_board(self, board, color):
-        # TODO: implement Dijkstra
-        # For each color
-        #   source_positions = positions of color on the source border
-        #   paths = [find_shortest_path_to_border(board, color, position_source_border) for position in source_positions]
-        #   shortest_path =  min(paths)
-        # return shortest_path_red - shortest_path_blue
         if self.eval_method == 'dijkstra':
-            return np.random.uniform(0, 1)
+            player_sp = self.find_shortest_path_to_border(board, color)
+            opponent_sp = self.find_shortest_path_to_border(board, color)
+
+            return player_sp - opponent_sp
         else:
             return np.random.uniform(0, 1)
-
-    def find_shortest_path_to_border(self, board, color, from_coordinate):
-        # Only count nodes without placed positions of this color
-        return 0
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Minimax for Hex")
-    parser.add_argument('--simulate', action='store_true',
-                        help='If added, simulates both sides')
-    parser.add_argument('--size', type=int, default=4,
-                        help='Set the board size')
-    parser.add_argument('--depth', type=int, default=3,
-                        help='Set the search depth')
-    parser.add_argument('--eval', choices=['dijkstra', 'random'],
-                        default='dijkstra', help='Choose the evaluation method')
+    parser.add_argument('--simulate', action='store_true', help='If added, simulates both sides')
+    parser.add_argument('--size', type=int, default=4, help='Set the board size')
+    parser.add_argument('--depth', type=int, default=3, help='Set the search depth')
+    parser.add_argument('--eval', choices=['dijkstra', 'random'], default='dijkstra', help='Choose the evaluation method')
     args = parser.parse_args(sys.argv[1:])
 
     hex_minimax = HexMinimax(args.size, args.depth, args.eval)
