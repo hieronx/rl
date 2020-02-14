e9a9ed2c194dafb4bf6efe310b1b86e7c86455ad
Mees Gelein <meesgelein@gmail.com>
2020-02-14 17:30:38 +0100

    Fixed minor things in evaluation

--- a1/search/evaluate.py
+++ a1/search/evaluate.py
@@ -45,10 +45,6 @@ class Evaluate:
                 neighbor_node = Node(neighbor, node.distance + cost)
                 neighbor_node.prev_node = node
 
-                if neighbor == to_coord: 
-                    end_node = neighbor_node
-                    break
-
                 found = False
                 for other_node in open_list:
                     if other_node.coord == neighbor_node.coord:
@@ -75,18 +71,23 @@ class Evaluate:
             # we're done with this node, add it to the closed list
             closed_list.append(node)
         
+        for node in closed_list:
+            if node.coord == to_coord:
+                end_node = node
+        for node in open_list:
+            if node.coord == to_coord:
+                end_node = node
+
         # return the path between these two points
         if end_node == None: return math.inf
         return end_node.distance
 
     def get_cost_between(self, board, coordA, coordB):
         colorA = board.get_color(coordA)
-        colorB = board.get_color(coordB)
-        if colorA == board.EMPTY or colorB == board.EMPTY:
+        if colorA == board.EMPTY: 
             return 1
-        if colorA == colorB:
+        else:
             return 0
-        return 1
 
     def find_lowest_dist_node(self, open_list):
         record = open_list[0]
@@ -104,7 +105,7 @@ class Evaluate:
             player_sp = self.find_shortest_path_to_border(board, color)
             opponent_sp = self.find_shortest_path_to_border(board, board.get_opposite_color(color))
 
-            # print("Found %d for player and %d for opponent" % (player_sp, opponent_sp))
+            print("Found %d for player and %d for opponent" % (player_sp, opponent_sp))
             return player_sp - opponent_sp
         else:
             return np.random.uniform(0, 1)
