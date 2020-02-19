8a170e4bfbbc29f2303ddbb08a18624d7e8c9bca
Mees Gelein <meesgelein@gmail.com>
2020-02-19 17:37:56 +0100

    Work in progress stuff on rewriting dijkstra

--- a1/search/evaluate.py
+++ a1/search/evaluate.py
@@ -24,81 +24,64 @@ class Evaluate:
                 if board.get_color(from_coord) == opposite_color or board.get_color(to_coord) == opposite_color:
                     continue
                 # Only count nodes without placed positions of this color
-                score = self.get_path_between(board, from_coord, to_coord, color)
+                score = self.get_path_length_between(board, from_coord, to_coord, color)
 
                 if score < min_score:
                     min_score = score
 
         return min_score
 
-    def get_path_between(self, board, from_coord, to_coord, color):
-        open_list = [Node(from_coord, 0)]
-        closed_list = []
-        end_node = None
+    def get_path_length_between(self, board, from_coord, to_coord, color):
+        dist, prev = self.dijkstra(board, from_coord, to_coord, color)
+        path = []
+        node = to_coord
 
-        while len(open_list) > 0:
-            # find the node in the open_list with the lowest distance
-            node = self.find_lowest_dist_node(open_list)
-            open_list.remove(node)
+        while node != None:
+            path.append(node)
+            node = prev[node]
 
-            neighbors = board.get_traversable_neighbors(node.coord, color)
-            for neighbor in neighbors:
-                
-                cost = self.get_cost_between(board, neighbor, node.coord)
+        return len(path)
 
-                neighbor_node = Node(neighbor, node.distance + cost)
-                neighbor_node.prev_node = node
-
-                found = False
-                for other_node in open_list:
-                    if other_node.coord == neighbor_node.coord:
-                        found = True
-                        if other_node.distance > neighbor_node.distance:
-                            other_node.distance = neighbor_node.distance
-                            other_node.prev_node = neighbor_node.prev_node
-                            
-                
-                # we already have an entry for this coord, so no need to continue this
-                
-                for other_node in closed_list:
-                    if other_node.coord == neighbor_node.coord:
-                        found = True
-                        if other_node.distance > neighbor_node.distance:
-                            other_node.distance = neighbor_node.distance
-                            other_node.prev_node = neighbor_node.prev_node
-                            
-                
-                # if we make it this far, please add this neighbor to the open_list
-                if not found: 
-                    open_list.append(neighbor_node)
-                
-            # we're done with this node, add it to the closed list
-            closed_list.append(node)
+    def dijkstra(self, board, from_coord, to_coord, color):
+        opposite_color = board.get_opposite_color(color)
+        q = []
+        dist = {}
+        prev = {}
         
-        for node in closed_list:
-            if node.coord == to_coord:
-                end_node = node
-        for node in open_list:
-            if node.coord == to_coord:
-                end_node = node
+        for node in board.board:
+            # don't add this node to the list, since we can basically see it as a wall
+            if board.get_color(node) == opposite_color:
+                continue
+            # otherwise initialize node as normal
+            dist[node] = math.inf
+            prev[node] = None
+            q.append(node)
+        dist[from_coord] = 0
 
-        # return the path between these two points
-        if end_node == None: return math.inf
-        return end_node.distance
+        while len(q) > 0:
+            node = min(q, key=lambda x: dist[x])
+            q.remove(node)
 
-    def get_cost_between(self, board, coordA, coordB):
-        colorA = board.get_color(coordA)
-        if colorA == board.EMPTY: 
-            return 1
-        else:
+            for neighbor in board.get_traversable_neighbors(node, color):
+                if neighbor not in dist: continue
+                
+                new_dist = dist[node] + self.distance_between(board, node, neighbor)
+                if new_dist < dist[neighbor]:
+                    dist[neighbor] = new_dist
+                    prev[neighbor] = node
+        
+        return (dist, prev)
+
+    def distance_between(self, board, coord_a, coord_b):
+        color_a = board.get_color(coord_a)
+        color_b = board.get_color(coord_b)
+        is_identical = color_a == color_b
+
+        if color_a != board.EMPTY and is_identical:
             return 0
-
-    def find_lowest_dist_node(self, open_list):
-        record = open_list[0]
-        for node in open_list:
-            if node.distance <= record.distance:
-                record = node
-        return record
+        else:
+            return 1
+        
 
     def evaluate_board(self, board, color):
         if board.check_draw(): return 0
@@ -115,10 +98,9 @@ class Evaluate:
             return np.random.uniform(0, 1)
 
 class Node:
-    prev_node = None
-    distance = math.inf
+    dist = math.inf
+    prev = None
     coord = None
 
-    def __init__(self, location, dist = math.inf):
-        self.coord = location
-        self.distance = dist
+    def __init__(self, coordinate):
+        self.coord = coordinate
