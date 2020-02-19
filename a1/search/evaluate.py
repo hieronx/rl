066e0ad11ab18a91110f703736b6f5c43f82e95b
Mees Gelein <meesgelein@gmail.com>
2020-02-19 17:46:18 +0100

    Stupid mistake

--- a1/search/evaluate.py
+++ a1/search/evaluate.py
@@ -51,7 +51,7 @@ class Evaluate:
             node = min(q, key=lambda x: dist[x])
             q.remove(node)
 
-            for neighbor in board.get_neighbors(node, color):
+            for neighbor in board.get_neighbors(node):
                 new_dist = dist[node] + self.distance_between(board, node, neighbor, opposite_color)
                 if new_dist < dist[neighbor]:
                     dist[neighbor] = new_dist
