8186f40da42eb315c2d4d4280b1bbaf7a31e6cea
Jeroen Offerijns <jeroen@offerijns.com>
2020-02-26 16:39:06 +0100

    chore: update trueskill settings

--- a1/rating.py
+++ a1/rating.py
@@ -12,10 +12,10 @@ def run_trueskill(args):
     """Surprise, starts a trueskill comparison for each of the possible permutations of the input players, using multi-threading"""
     freeze_support() # for Windows support
 
-    board_size = 4
-    game_cnt = 100
+    board_size = 3
+    game_cnt = 200
     # players = [{ 'depth': 3, 'eval': 'random' }, { 'depth': 3, 'eval': 'Dijkstra' }, { 'depth': 4, 'eval': 'Dijkstra' }]
-    players = [{ 'depth': 3, 'time_limit': None, 'eval': 'random' }, { 'depth': None, 'time_limit': 0.1, 'eval': 'Dijkstra' }]
+    players = [{ 'depth': 3, 'time_limit': None, 'eval': 'random' }, { 'depth': 3, 'time_limit': None, 'eval': 'Dijkstra' }]
 
     player_permutations = []
     for i in range(len(players)):
