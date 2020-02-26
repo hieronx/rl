975cd5177e758a9c9d92644186f45053173d4f8b
Mees Gelein <meesgelein@gmail.com>
2020-02-26 16:26:57 +0100

    more comments and removed an unncessary import

--- a1/util.py
+++ a1/util.py
@@ -1,7 +1,7 @@
 from os import system, name
 
 def cls():
-    """Clears the screen, depending on the OS level call"""
+    """Clears the screen, depending on the OS level call, this is merely a small utility function"""
     if name == 'nt':
         temp = system('cls')
     else:
