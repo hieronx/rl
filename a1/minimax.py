ba8eac9e15173347b4599d75ea29ba81c444df9f
Mees Gelein <meesgelein@gmail.com>
2020-02-26 17:14:48 +0100

    Minimax should now drop everything like its hot when time has passed

--- a1/minimax.py
+++ a1/minimax.py
@@ -18,27 +18,31 @@ class Minimax:
         self.evaluate = evaluate_class
         self.live_play = live_play
         self.disable_tt = disable_tt
+        self.start_time = 0
 
         self.tp_table = {}
         self.stats = { 'nodes_searched': 0, 'cutoffs': 0, 'tt_lookups': 0 }
 
     def get_next_move(self, board, color):
         """Returns the best next move for the provided color on the provided board/state"""
-        start_time = time.time()
+        self.start_time = time.time()
         alpha = -math.inf
         beta = math.inf
         opposite_color = board.get_opposite_color(color)
 
         max_depth = self.depth or 1
         if self.depth:
-            move, _ = self.alpha_beta_search(board, self.depth, color, opposite_color, alpha, beta, True)
+            best_move, _ = self.alpha_beta_search(board, self.depth, color, opposite_color, alpha, beta, True)
         elif self.time_limit:
-            while (time.time() - start_time) < self.time_limit:
-                move, _ = self.alpha_beta_search(board, max_depth, color, opposite_color, alpha, beta, True)
+            best_move = None
+            while (time.time() - self.start_time) < self.time_limit:
+                new_move, _ = self.alpha_beta_search(board, max_depth, color, opposite_color, alpha, beta, True)
+                if new_move is not None:
+                    best_move = new_move
                 max_depth += 1
         
         if self.live_play:
-            elapsed_time = time.time() - start_time
+            elapsed_time = time.time() - self.start_time
 
             cls()
             print("Searched to depth %d, evaluated %d nodes, experienced %d cutoffs, and used %d TT lookups." % (max_depth, self.stats['nodes_searched'], self.stats['cutoffs'], self.stats['tt_lookups']))
@@ -46,11 +50,15 @@ class Minimax:
         
         self.stats = { 'nodes_searched': 0, 'cutoffs': 0, 'tt_lookups': 0 }
 
-        return move
+        return best_move
 
     def alpha_beta_search(self, board, depth, color, opposite_color, alpha, beta, maximizing):
         """Handles minimax search using alpha beta pruning and good use of move-ordering and transposition tables"""
         cached_best_move = None
+
+        # stop handling any code and immediately drop all your responsibilites if times has passed
+        if self.time_limit is not None and time.time() - self.start_time >= self.time_limit: 
+            return (None, 0)
         
         if not self.disable_tt:
             hash_code = board.hash_code(color if maximizing else opposite_color)
