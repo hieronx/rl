7415f37c3df1af05054de70d8c905d86fe43470b
Jeroen Offerijns <jeroen@offerijns.com>
2020-02-26 15:32:55 +0100

    feat: remove part 4 code from part 3

---
+++ a1/3-search/game.py
@@ -0,0 +1,57 @@
+import math
+import numpy as np
+
+from util import cls
+from hexboard import HexBoard
+from minimax import Minimax
+from evaluate import Evaluate
+import time
+
+char_to_row_idx = {'a': 0, 'b': 1, 'c': 2, 'd': 3}
+
+class HexGame:
+
+    def __init__(self, size, depth):
+        self.board_size = size
+        self.search_depth = depth
+        self.minimax = Minimax(size, depth, Evaluate())
+
+    def run_interactively(self, board):
+        while not board.game_over():
+            print("Waiting for CPU move...")
+            move = self.minimax.get_next_move(board, HexBoard.RED)
+            board = board.make_move(move, HexBoard.RED)
+            board.print()
+            print('\n')
+
+            if board.game_over():
+                break
+
+            while True:
+                move = input("Your move: ")
+
+                if len(move) == 2:
+                    x, y = move
+                    if (x in char_to_row_idx and y.isdigit):
+                        if board.is_empty((char_to_row_idx[x], int(y))):
+                            break
+
+            board = board.make_move((char_to_row_idx[x], int(y)), HexBoard.BLUE)
+            print(board.game_over())
+
+        if board.check_win(HexBoard.RED):
+            print('The AI won.')
+        else:
+            print('You won.')
+
+    def simulate(self, board):
+        while not board.game_over():
+            board.place(self.minimax.get_next_move(board, HexBoard.RED), HexBoard.RED)
+            board.place(self.minimax.get_next_move(board, HexBoard.BLUE), HexBoard.BLUE)
+
+        if board.check_win(HexBoard.RED):
+            print('Red won.')
+        else:
+            print('Blue won.')
+
+        board.print()
