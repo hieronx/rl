9a166a5646a2649255e204992934ca9ca2365e57
Jeroen Offerijns <jeroen@offerijns.com>
2020-02-26 16:14:40 +0100

    refactor: remove separate parts

--- a1/5-experiment/util.py
+++
@@ -1,8 +0,0 @@
-from os import system, name
-
-def cls():
-    """Clears the screen, depending on the OS level call"""
-    if name == 'nt':
-        temp = system('cls')
-    else:
-        temp = system('clear')
--- a1/main.py
+++ a1/main.py
@@ -80,6 +80,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Minimax for Hex")
     parser.add_argument('--trueskill', action='store_true', help='If added, evaluate using TrueSkill')
     parser.add_argument('--simulate', action='store_true', help='If added, simulates both sides')
+    parser.add_argument('--disable-tt', action='store_true', help='If added, disables the transposition table')
     parser.add_argument('--size', type=int, default=4, help='Set the board size')
     parser.add_argument('--depth', type=int, default=4, help='Set the search depth')
     parser.add_argument('--eval', choices=['Dijkstra', 'random'], default='Dijkstra', help='Choose the evaluation method')
