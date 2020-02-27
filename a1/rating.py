99e4a64411e71dca3a0fe2086240582b912ef9e0
Mees Gelein <meesgelein@gmail.com>
2020-02-27 11:59:54 +0100

    Recreated trueskill tests for random vs AStar

--- a1/rating.py
+++ a1/rating.py
@@ -13,15 +13,15 @@ def run_trueskill(args):
     """Surprise, starts a trueskill comparison for each of the possible permutations of the input players, using multi-threading"""
     freeze_support() # for Windows support
 
-    board_size = 5
-    game_cnt = 20
+    board_size = 3
+    game_cnt = 200
     # players = [
     #     { 'depth': 3, 'time_limit': None, 'eval': 'random' },
     #     { 'depth': 3, 'time_limit': None, 'eval': 'Dijkstra' },
     #     { 'depth': 4, 'time_limit': None, 'eval': 'Dijkstra' },
     #     { 'depth': None, 'time_limit': 0.1, 'eval': 'Dijkstra' },
     # ]
-    players = [{ 'depth': None, 'time_limit': 1, 'eval': 'Dijkstra' }, { 'depth': None, 'time_limit': 1, 'eval': 'AStar' }]
+    players = [{ 'depth': 3, 'time_limit': None, 'eval': 'random' }, { 'depth': 3, 'time_limit': None, 'eval': 'AStar' }]
 # 
     player_permutations = []
     for i in range(len(players)):
