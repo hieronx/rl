a7bccfd74082e097bf76e549b6dc59598c404af1
Jeroen Offerijns <jeroen@offerijns.com>
2020-03-04 17:43:25 +0100

    refactor: move tests into separate modules

---
+++ a2/search/test.py
@@ -0,0 +1,66 @@
+import unittest
+import random
+import time
+
+from util.hexboard import HexBoard
+from search.minimax import Minimax
+from evaluate.dijkstra import Dijkstra
+from evaluate.astar import AStar
+
+class TestSearch(unittest.TestCase):
+    """All unit tests for search used to determine if the code we wrote is still running as intended"""
+
+    def test_minimax(self):
+        """"Tests to see if minimax returns the expected best moves for specific board states"""
+        dijkstra = Dijkstra()
+        board = HexBoard(3)
+
+        board.board[(0, 0)] = HexBoard.RED
+        board.board[(0, 1)] = HexBoard.RED
+
+        self.assertFalse(board.game_over())
+
+        minimax = Minimax(3, 3, None, dijkstra, False)
+        move = minimax.get_next_move(board, HexBoard.RED)
+        self.assertEqual(move, (0, 2))
+
+    def test_minimax_top_left(self):
+        """"Another scenario which tests a specific minimax scenario"""
+        dijkstra = Dijkstra()
+        board = HexBoard(4)
+
+        board.board[(1, 0)] = HexBoard.RED
+        board.board[(1, 1)] = HexBoard.RED
+        board.board[(1, 2)] = HexBoard.RED
+        board.board[(0, 1)] = HexBoard.BLUE
+        board.board[(0, 2)] = HexBoard.BLUE
+        board.board[(0, 3)] = HexBoard.BLUE
+
+        good_board = board.make_move((1, 3), HexBoard.RED)
+        eval_good_board = dijkstra.evaluate_board(good_board, HexBoard.RED)
+
+        bad_board = board.make_move((0, 0), HexBoard.RED)
+        eval_bad_board = dijkstra.evaluate_board(bad_board, HexBoard.RED)
+
+        self.assertTrue(eval_good_board > eval_bad_board)
+
+        minimax = Minimax(4, 3, None, dijkstra, False)
+        move = minimax.get_next_move(board, HexBoard.RED)
+        self.assertEqual(move, (1, 3))
+
+    def test_tp_table(self):
+        """Tests if the transposition table is working as intended"""
+        dijkstra = Dijkstra()
+        board = HexBoard(3)
+        minimax = Minimax(3, 3, None, dijkstra, False)
+
+        board.board[(0, 0)] = HexBoard.RED
+        board.board[(0, 1)] = HexBoard.RED
+        self.assertEqual(minimax.tp_table, {})
+        
+        move = minimax.get_next_move(board, HexBoard.RED)
+        self.assertTrue(len(minimax.tp_table) > 0)
+    
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ a2/util/test.py
@@ -0,0 +1,74 @@
+import unittest
+import random
+import time
+
+from util.hexboard import HexBoard
+from search.minimax import Minimax
+from evaluate.dijkstra import Dijkstra
+from evaluate.astar import AStar
+
+class TestUtil(unittest.TestCase):
+    """All unit tests for util used to determine if the code we wrote is still running as intended"""
+
+    def test_win_detection(self):
+        """Checks if the detection for win states is working as expected by creating a win scenario"""
+        for i in range(0, 2):
+            winner = HexBoard.RED if i == 0 else HexBoard.BLUE
+            looser = HexBoard.BLUE if i == 0 else HexBoard.RED
+            board = HexBoard(3)
+
+            board.board[(1, 1)] = looser
+            board.board[(2, 1)] = looser
+            board.board[(1, 2)] = looser
+            board.board[(2, 2)] = looser
+            board.board[(0, 0)] = winner
+            board.board[(1, 0)] = winner
+            board.board[(2, 0)] = winner
+            board.board[(0, 1)] = winner
+            board.board[(0, 2)] = winner
+
+            self.assertEqual(board.check_win(winner), True)
+            self.assertEqual(board.check_win(looser), False)
+
+    def test_game_end(self):
+        """Checks if the detection for gameover states is working as expected by filling all hexes with one color"""
+        endable_board = HexBoard(4)
+
+        while not endable_board.game_over():
+            endable_board.board[(random.randint(0, 4), random.randint(0, 4))] = HexBoard.RED
+
+        self.assertEqual(endable_board.game_over(), True)
+        self.assertEqual(endable_board.check_win(HexBoard.RED), True)
+        self.assertEqual(endable_board.check_win(HexBoard.BLUE), False)
+
+    def test_source_coordinates(self):
+        """Let's see if the source coordinates are all correct and as we think they should be"""
+        board = HexBoard(4)
+        source_coordinates = board.get_source_coordinates(HexBoard.RED)
+        actual_sources = [(0, 0), (1, 0), (2, 0), (3, 0)]
+
+        for actual_source in actual_sources:
+            self.assertTrue(actual_source in source_coordinates)
+
+    def test_target_coordinates(self):
+        """Does the same thing as test_source coordinates, only checks for the target coordinates"""
+        board = HexBoard(4)
+        target_coordinates = board.get_target_coordinates(HexBoard.RED)
+        actual_targets = [(0, 3), (1, 3), (2, 3), (3,3)]
+
+        for actual_target in actual_targets:
+            self.assertTrue(actual_target in target_coordinates)
+
+    def test_hash_code(self):
+        """Makes sure the hashcode that we're generating is indeed what we expect it to be"""
+        board = HexBoard(3)
+
+        board.board[(0, 0)] = HexBoard.RED
+        board.board[(1, 2)] = HexBoard.BLUE
+
+        self.assertEqual(board.hash_code(HexBoard.BLUE), 3331333321)
+        self.assertEqual(board.hash_code(HexBoard.RED), 3331333322)
+
+
+if __name__ == '__main__':
+    unittest.main()
