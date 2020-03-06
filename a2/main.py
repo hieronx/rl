36774b382af5b5b8461a1b89b08a9193690026aa
Jeroen Offerijns <jeroen@offerijns.com>
2020-03-06 12:11:28 +0100

    refactor: add command subparsers

--- a2/main.py
+++ a2/main.py
@@ -15,33 +15,43 @@ logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Reinforcement learning for Hex")
-    parser.add_argument('task', choices=['play', 'trueskill', 'benchmark'])
-    parser.add_argument('--config', choices=configs.keys(), default=None, help='If added, evaluate using TrueSkill using the chosen configuration set')
-    parser.add_argument('--plot', action='store_true', help='If added, save the plots of the TrueSkill evaluations')
-    parser.add_argument('--disable-tt', action='store_true', help='If added, disables the transposition table')
-    parser.add_argument('--search', choices=['Minimax', 'MCTS'], default='MCTS', help='Set the search method')
-    parser.add_argument('--size', type=int, default=4, help='Set the board size')
-    parser.add_argument('--depth', type=int, default=None, help='Set the search depth')
-    parser.add_argument('--time-limit', type=float, default=None, help='Set the time limit')
-    parser.add_argument('--eval', choices=['Dijkstra', 'random', 'AStar'], default='Dijkstra', help='Choose the evaluation method')
+    subparsers = parser.add_subparsers(dest='command')
+
+    play = subparsers.add_parser('play', help='Play against the RL algorithm')
+    play.add_argument('--disable-tt', action='store_true', help='If added, disables the transposition table')
+    play.add_argument('--search', choices=['Minimax', 'MCTS'], default='MCTS', help='Set the search method')
+    play.add_argument('--size', type=int, default=4, help='Set the board size')
+    play.add_argument('--depth', type=int, default=None, help='Set the search depth')
+    play.add_argument('--time-limit', type=float, default=None, help='Set the time limit')
+    play.add_argument('--eval', choices=['Dijkstra', 'random', 'AStar'], default='Dijkstra', help='Choose the evaluation method')
+
+    trueskill = subparsers.add_parser('trueskill', help='Evaluate the RL algorithm using TrueSkill')
+    trueskill.add_argument('--config', choices=configs.keys(), default=None, help='If added, evaluate using TrueSkill using the chosen configuration set')
+    trueskill.add_argument('--plot', action='store_true', help='If added, save the plots of the TrueSkill evaluations')
+    trueskill.add_argument('--disable-tt', action='store_true', help='If added, disables the transposition table')
+
+    benchmark = subparsers.add_parser('benchmark', help='Run a standardized benchmarking script')
+
     args = parser.parse_args(sys.argv[1:])
 
-    if args.task == 'trueskill' and not args.config:
-        logger.warn('--config needs to be set.')
-        exit()
+    if args.command == 'play':
+        if args.search == 'Minimax' and not (args.depth or args.time_limit):
+            logger.warn('Either depth or time limit needs to be set when using Minimax search.')
+            exit()
 
-    if args.search == 'Minimax' and not (args.depth or args.time_limit):
-        logger.warn('Either depth or time limit needs to be set when using Minimax search.')
-        exit()
+        if args.depth and args.time_limit:
+            logger.warn('Depth and time limit cannot both be set.')
+            exit()
 
-    if args.depth and args.time_limit:
-        logger.warn('Depth and time limit cannot both be set.')
-        exit()
+    if args.command == 'trueskill':
+        if not args.config:
+            logger.warn('--config needs to be set.')
+            exit()
 
-    if args.task == 'trueskill':
+    if args.command == 'trueskill':
         logger.info('Booting TrueSkill rating script...')
         run_trueskill(args)
-    elif args.task == 'benchmark':
+    elif args.command == 'benchmark':
         logger.info('Booting benchmark script...')
         run_benchmark()
     else:
