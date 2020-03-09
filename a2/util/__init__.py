030cfa9d589bb9943caa9da17798febe373ee052
Jeroen Offerijns <jeroen@offerijns.com>
2020-03-09 12:41:44 +0100

    feat: dynamically calculate progress bar width

--- a2/util/__init__.py
+++ a2/util/__init__.py
@@ -10,13 +10,19 @@ def cls():
         temp = os.system('clear')
 
 # Based on https://stackoverflow.com/questions/3160699/python-progress-bar
-def progressbar(it, desc="", position=None, size=80, total=None, file = sys.stdout):
+def progressbar(it, desc="", position=None, size=None, total=None, file = sys.stdout):
+
     count = total or len(it)
 
     def show(j, ips):
-        x = int(size*j/count)
+        _, columns = os.popen('stty size', 'r').read().split()
+
+        # Terminal width - description length - spacing - completed count - spacing - total count - spacing - ips
+        width = size or (int(columns) - len(desc) - 5 - len(str(j)) - 1 - len(str(count)) - 1 - 8) 
+        
+        x = int(width * j / count)
         if position: moveto(file, position)
-        file.write("%s: |%s%s| %i/%i %.2fit/s\r" % (desc, "█"*x, " "*(size-x), j, count, ips))
+        file.write("%s: |%s%s| %i/%i %.2fit/s\r" % (desc, "█" * x, " " * (width - x), j, count, ips))
         file.flush()
         if position: moveto(file, -position)
 
