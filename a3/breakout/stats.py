2f6de3eeffb64b03b846aed7721d68441ed0774a
Jeroen Offerijns <jeroen@offerijns.com>
2020-04-17 11:47:44 +0200

    fix: variable references

--- a3/breakout/stats.py
+++ a3/breakout/stats.py
@@ -1,6 +1,7 @@
 import statistics
 from collections import deque
 
+
 class Stats:
     """This class holds some simple utility methods for generating averages and summaries of game performance"""
 
@@ -14,12 +15,12 @@ class Stats:
     def finished_game(self):
         """Finishes a game, updating statistics and resetting current game score"""
         self.num_games_played += 1
-        self.running_game_scores.append(current_game_score)
-        self.total_game_score += current_game_score
+        self.running_game_scores.append(self.current_game_score)
+        self.total_game_score += self.current_game_score
         self.print_summary()
         self.current_game_score = 0
 
     def print_summary(self):
         """Prints a summary of the statistics obtained during this run so far to the console"""
-        summary = (num_games_played, current_game_score, statistics.mean(running_game_scores))
+        summary = (self.num_games_played, self.current_game_score, statistics.mean(self.running_game_scores))
         print('Ended game %d with score %d, running average is %.2f' % summary)
