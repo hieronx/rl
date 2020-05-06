7d707dab051a28934fec3dee8fddb0fb26685756
MGelein <meesgelein@gmail.com>
2020-05-06 10:41:25 +0200

    changing hyperparameters

--- a4/alphazero/train.py
+++ a4/alphazero/train.py
@@ -5,22 +5,22 @@ from alphazero.utils import *
 
 args = dotdict({
     'numIters': 100,
-    'numEps': 50,              # Number of complete self-play games to simulate during a new iteration.
+    'numEps': 400,              # Number of complete self-play games to simulate during a new iteration.
     'tempThreshold': 15,        #
     'updateThreshold': 0.6,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
     'maxlenOfQueue': 200000,    # Number of game examples to train the neural networks.
     'numMCTSSims': 25,          # Number of games moves for MCTS to simulate.
-    'arenaCompare': 50,         # Number of games to play during arena play to determine if new net will be accepted.
+    'arenaCompare': 40,         # Number of games to play during arena play to determine if new net will be accepted.
     'cpuct': 1,
 
-    'checkpoint': './temp/',
+    'checkpoint': './temp/5x5/',
     'load_model': False,
     'load_folder_file': ('/dev/models/8x100x50','best.pth.tar'),
     'numItersForTrainExamplesHistory': 20,
 })
 
 def train():
-    g = Game(7)
+    g = Game(5)
     nnet = nn(g)
 
     # if args.load_model:
