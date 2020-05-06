d0386e81e7d260f2bf4023687fb310e8424921d4
MGelein <meesgelein@gmail.com>
2020-05-06 11:20:14 +0200

    Trying to fix bugs during training

--- a4/alphazero/train.py
+++ a4/alphazero/train.py
@@ -5,7 +5,7 @@ from alphazero.utils import *
 
 args = dotdict({
     'numIters': 100,
-    'numEps': 100,              # Number of complete self-play games to simulate during a new iteration.
+    'numEps': 20,              # Number of complete self-play games to simulate during a new iteration.
     'tempThreshold': 15,        #
     'updateThreshold': 0.6,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
     'maxlenOfQueue': 200000,    # Number of game examples to train the neural networks.
