ece4ce3bcce55395c432987d6ad98b632e322ec5
Jeroen Offerijns <jeroen@offerijns.com>
2020-05-12 16:35:14 +0200

    feat: remove saving and overwriting old models

--- a4/alpha-zero-torch/src/trainer.py
+++ a4/alpha-zero-torch/src/trainer.py
@@ -17,8 +17,6 @@ class AlphaZeroTrainer(object):
 		self.game = game
 		self.mcts = mcts
 		self.queue_len = params['queue_len']
-		self.arena_compare = params['arena_compare']
-		self.update_threshold = params['update_threshold']
 		self.n_games = params['n_games']
 		self.iterations = params['iterations']
 		self.temp = params['temp']
@@ -31,43 +29,18 @@ class AlphaZeroTrainer(object):
 		# Save the initial model before any training
 		self.nn_wrapper.save_model("models", "%d.pt" % start_time)
 
-		for i in progressbar(range(self.iterations), desc="Training", position=0):
+		for i in progressbar(range(self.iterations), desc="Overall progress", position=0):
 			completed_games = 0
 			start_time = time.time()
 			print_progressbar(desc='Playing games', completed=0, start_time=start_time, total=self.n_games, position=1)
-			# pool.map(self.play_game, range(self.n_games))
 			for _ in pool.imap_unordered(self.play_game, range(self.n_games)):
 				completed_games += 1
 				print_progressbar(desc='Playing games', completed=completed_games, start_time=start_time, total=self.n_games, position=1)
 
 			loss = self.nn_wrapper.train(self.replay_buffer)
 
-			prev_nn_wrapper = ModelWrapper(game, device, **params)
-			prev_nn_wrapper.load_model("models/%d.pt" % start_time)
-			
-			prev_wins, new_wins = 0, 0
-			for j in progressbar(range(self.arena_compare), desc="Playing matchups", position=1):
-				if j % 2 == 0:
-					winner = self.play_matchup(prev_nn_wrapper, self.nn_wrapper)
-				else:
-					winner = self.play_matchup(self.nn_wrapper, prev_nn_wrapper)
-
-				if winner == 1:
-					prev_wins += 1
-				elif winner == -1:
-					new_wins += 1
-			
-			win_perc = new_wins / self.arena_compare
-
-			if win_perc > self.update_threshold:
-				self.nn_wrapper.save_model("models", "%d.pt" % start_time)
-				self.nn_wrapper.save_model("models", "best.pt")
-				print("Win perc = %.2f, overwriting previous model." % win_perc)
-			else:
-				self.nn_wrapper.load_model("models/%d.pt" % start_time)
-				print("Win perc = %.2f, keeping previous model." % win_perc)
-
-			print("Finished self-play iteration %d/%d, avg loss: %.2f" % (i+1, self.iterations, loss))
+			self.nn_wrapper.save_model("models", "%d.pt" % start_time)
+			# print("Finished self-play iteration %d/%d, avg loss: %.2f" % (i+1, self.iterations, loss))
 
 		return loss
 
