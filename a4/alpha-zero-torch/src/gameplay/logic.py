19bf4f614a76478568010483b482bc30a78b5dcf
MGelein <meesgelein@gmail.com>
2020-05-12 16:07:47 +0200

    Ready to test how the models compare

--- a4/alpha-zero-torch/src/gameplay/logic.py
+++ a4/alpha-zero-torch/src/gameplay/logic.py
@@ -28,15 +28,19 @@ def player_vs_player(game, p1, p2, n_games = 10, treshold = 0.5, print_b = False
 	
 	for i in progressbar(range(n_games), desc="Playing"):
 		print("Game: {}/{}".format(i,n_games))
-		winner = play_game(game = game, p1 = p1, p2 = p2, print_b = print_b) #todo: we should probabily vary who plays first, some games are biased torwards first players
-		print(winner)
+		reverse_players = i % 2 == 0
+
+		
+		winner = play_game(game = game, p1 = p2 if reverse_players else p1, p2 = p1 if reverse_players else p2, print_b = print_b) #todo: we should probabily vary who plays first, some games are biased torwards first players
 		if winner == 0:
 			draws += 1
 		elif winner == 1:
 			wins_p1 +=1
+		elif winner == -1 and reverse_players:
+			wins_p1 += 1
 
 	print("Player 1 won:{}%, lost:{}%, drew:{}%".format(wins_p1, n_games - wins_p1 - draws, draws))
 
 	winner_model = p1 if wins_p1/n_games > treshold else p2
 
-	return winner
+	return winner_model
