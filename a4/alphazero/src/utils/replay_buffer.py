23bd10e25aee6f498c8a51f6ab9fa83ec40ea0b8
MGelein <meesgelein@gmail.com>
2020-05-13 10:20:00 +0200

    Commenting code for my rewrite

--- a4/alphazero/src/utils/replay_buffer.py
+++ a4/alphazero/src/utils/replay_buffer.py
@@ -2,15 +2,21 @@ import numpy as np
 from collections import deque
 
 class ReplayBuffer(object):
+	"""
+	This class holds the history for a specific (queue length) amount of games.
+	"""
+
 	def __init__(self, window_size):
+		"""Creates a new replaybuffer using the provided window size as the maximum size for the replay buffer"""
 		super(ReplayBuffer, self).__init__()
 		self.buffer = deque(maxlen=window_size)
-		self.window_size = window_size
 
 	def save_game(self, game):
+		"""Appends on game to the replaybuffer"""
 	    self.buffer.append(game)
 
 	def sample_batch(self, batch_size):
+		"""Samples a number of frames from randomly sampled games equal to batch size"""
 	    games = np.random.choice(
 	        self.buffer,
 	        size= batch_size)
@@ -20,4 +26,5 @@ class ReplayBuffer(object):
 	    return list(pos[:,0]), list(pos[:,1]), list(pos[:,2])
 
 	def get_total_positions(self):
+		"""Returns the complete length of ALL the frames in the replay buffer"""
 		return float(sum(len(g.history) for g in self.buffer))
