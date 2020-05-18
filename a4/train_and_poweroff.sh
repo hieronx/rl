45a4de237e6f9a8a7fada33054151e1847724eee
MGelein <meesgelein@gmail.com>
2020-05-19 00:09:31 +0200

    Minor naming error

--- a4/train_and_poweroff.sh
+++ a4/train_and_poweroff.sh
@@ -3,6 +3,6 @@ cp alphazero/config_150it.yaml alphazero/config.yaml
 python main.py train alphazero > output_150it.txt
 
 cp alphazero/config_da2.2.yaml alphazero/config.yaml
-python main.py train alphazero > output_150it.txt
+python main.py train alphazero > output_da2.2.txt
 
 poweroff
