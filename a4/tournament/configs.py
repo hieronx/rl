5b47f088ed0d34bef62a2ad8a3642f5c72ffcf7e
Mees Gelein <meesgelein@gmail.com>
2020-05-18 11:19:47 +0200

    Added all configs that should be run

--- a4/tournament/configs.py
+++ a4/tournament/configs.py
@@ -4,16 +4,43 @@ configs = {
         { 'id': 'mcts', 'search': 'mcts', 'depth': None, 'time_limit': 0.1, 'eval': 'Dijkstra', 'rave_k': -1 },
     ],
     '50_50_mcts_minimax': [
-        { 'id': 'AZ_50x50_1', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_50x50it_cp2_da1.1_q150.pt'},
-        { 'id': 'AZ_50x50_2', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_50x50it_cp2_da1.1_q150_2.pt'},
+        { 'id': 'AZ_50it_50g_1', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_50x50it_cp2_da1.1_q150.pt'},
+        { 'id': 'AZ_50it_50g_2', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_50x50it_cp2_da1.1_q150_2.pt'},
         { 'id': 'minimax', 'search': 'minimax', 'depth': None, 'time_limit': 0.1, 'eval': 'Dijkstra', 'rave_k': -1 },
         { 'id': 'mcts', 'search': 'mcts', 'depth': None, 'time_limit': 0.1, 'eval': 'Dijkstra', 'rave_k': -1 },
     ],
     '50_50_1000_mcts_minimax': [
-        { 'id': 'AZ_50x50_1', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_50x50it_cp2_da1.1_q150.pt'},
-        { 'id': 'AZ_50x50_2', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_50x50it_cp2_da1.1_q150_2.pt'},
+        { 'id': 'AZ_50it_50g_1', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_50x50it_cp2_da1.1_q150.pt'},
+        { 'id': 'AZ_50it_50g_2', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_50x50it_cp2_da1.1_q150_2.pt'},
+        { 'id': 'AZ_1000it_50g', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_519x50it_cp2_da1.1_q150.pt'},
         { 'id': 'minimax', 'search': 'minimax', 'depth': None, 'time_limit': 0.1, 'eval': 'Dijkstra', 'rave_k': -1 },
         { 'id': 'mcts', 'search': 'mcts', 'depth': None, 'time_limit': 0.1, 'eval': 'Dijkstra', 'rave_k': -1 },
+    ],
+    'training_length': [
+        { 'id': 'AZ_0it', 'search': 'alphazero', 'model_path': 'alphazero/tests/random.pt'},
+        { 'id': 'AZ_50it_50g', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_50x50it_cp2_da1.1_q150.pt'},
+        { 'id': 'AZ_150it_50g', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_150x50it_cp2_da1.1_q150.pt'},
+        { 'id': 'AZ_500it_50g', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_519x50it_cp2_da1.1_q150.pt'},
+        { 'id': 'AZ_1000it_50g', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_1000x50it_cp2_da1.1_q150.pt'},
+    ],
+    'cp': [
+        { 'id': 'AZ_50it_50g_cp0.4', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_50x50it_cp0.4_da1.1_q150.pt'},
+        { 'id': 'AZ_50it_50g_cp2', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_50x50it_cp2_da1.1_q150.pt'},
+        { 'id': 'AZ_50it_50g_cp5', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_50x50it_cp5_da1.1_q150.pt'},
+    ],
+    'replay_buffer': [
+        { 'id': 'AZ_50it_50g_rp15', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_50x50it_cp2_da1.1_q15.pt'},
+        { 'id': 'AZ_50it_50g_rp150', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_50x50it_cp2_da1.1_q150.pt'},
+        { 'id': 'AZ_50it_50g_rp1500', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_50x50it_cp2_da1.1_q1500.pt'},
+    ],
+    'temperature': [
+        { 'id': 'AZ_50it_50g_no_temp', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_50x50it_cp2_da1.1_q1500.pt'},
+        { 'id': 'AZ_50it_50g_temp0.2', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_50x50it_cp2_da1.1_q1500_temp0.2.pt'},
+    ],
+    'dirichlet_alpha': [
+        { 'id': 'AZ_50it_50g_da0.5', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_50x50it_cp2_da0.5_q150.pt'},
+        { 'id': 'AZ_50it_50g_da1.1', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_50x50it_cp2_da1.1_q150.pt'},
+        { 'id': 'AZ_50it_50g_da2.2', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_50x50it_cp2_da2.2_q150.pt'},
     ]
     
 }
