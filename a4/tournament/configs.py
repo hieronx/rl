2e9d7b0136806371fb76e151545d1cf794288ca4
Mees Gelein <meesgelein@gmail.com>
2020-05-18 11:09:20 +0200

    Renaming stupid variable names that I made up

--- a4/tournament/configs.py
+++ a4/tournament/configs.py
@@ -8,5 +8,12 @@ configs = {
         { 'id': 'AZ_50x50_2', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_50x50it_cp2_da1.1_q150_2.pt'},
         { 'id': 'minimax', 'search': 'minimax', 'depth': None, 'time_limit': 0.1, 'eval': 'Dijkstra', 'rave_k': -1 },
         { 'id': 'mcts', 'search': 'mcts', 'depth': None, 'time_limit': 0.1, 'eval': 'Dijkstra', 'rave_k': -1 },
+    ],
+    '50_50_1000_mcts_minimax': [
+        { 'id': 'AZ_50x50_1', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_50x50it_cp2_da1.1_q150.pt'},
+        { 'id': 'AZ_50x50_2', 'search': 'alphazero', 'model_path': 'alphazero/tests/new_50x50it_cp2_da1.1_q150_2.pt'},
+        { 'id': 'minimax', 'search': 'minimax', 'depth': None, 'time_limit': 0.1, 'eval': 'Dijkstra', 'rave_k': -1 },
+        { 'id': 'mcts', 'search': 'mcts', 'depth': None, 'time_limit': 0.1, 'eval': 'Dijkstra', 'rave_k': -1 },
     ]
+    
 }
