ad006b0ecd8e7d94bd8d6a465e50aaeb74598789
MGelein <meesgelein@gmail.com>
2020-05-19 16:47:56 +0200

    changed shell script and modified new graph

--- a4/run_tournament.sh
+++ a4/run_tournament.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 python main.py tournament --max-threads 2 --sigma-threshold 0.85 --config 50_50_temp_mcts_minimax
+python main.py tournament --max-threads 2 --sigma-threshold 0.85 --config 150temp_mcts_minimax
